7320c2dbfbffa882efeedfa9e095abad042aae8c
ishi <berugen1st@gmail.com>
2023-09-26 12:35:52 +0700

    adjust border color for node creator

--- solid-client/src/components/NodeCreator.tsx
+++ solid-client/src/components/NodeCreator.tsx
@@ -5,7 +5,7 @@ export function NodeCreator({ onAdd }: Partial<TNodeCreator>) {
   return (
     <div
       onclick={onAdd}
-      class='grid place-items-center w-72 h-72 m-2 rounded-lg border-dashed border border-b-disabled bg-white hover:bg-bnt-white-1 dark:bg-bnt-dark dark:hover:bg-bnt-dark-1 cursor-pointer transition-all'
+      class='grid place-items-center w-72 h-72 m-2 rounded-lg border-dashed border border-bnt-dark-2 bg-white hover:bg-bnt-white-1 dark:bg-bnt-dark dark:hover:bg-bnt-dark-1 cursor-pointer transition-all'
     >
       <PlusIcon />
     </div>

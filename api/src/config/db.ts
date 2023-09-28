c0c885f7a98d6ed45b88ab3e0147f3c91341f431
ishi <berugen1st@gmail.com>
2023-09-29 02:35:59 +0700

    remove partial parameters in drizzle hook

--- api/src/config/db.ts
+++ api/src/config/db.ts
@@ -1,19 +1,14 @@
 import * as schema from "../db/schema";
 import { drizzle } from "drizzle-orm/libsql";
-import { createClient, Config } from "@libsql/client";
+import { createClient } from "@libsql/client";
 
 const DB_NAME = process.env.TURSO_URL;
 const TURSO_AUTH_TOKEN = process.env.TURSO_AUTH_TOKEN;
 
-export function useDrizzle({
-  url = DB_NAME as string,
-  authToken = TURSO_AUTH_TOKEN,
-  ...rest
-}: Config) {
+export function useDrizzle() {
   const client = createClient({
-    url,
-    authToken,
-    ...rest,
+    url: DB_NAME as string,
+    authToken: TURSO_AUTH_TOKEN,
   });
   return drizzle(client, { schema });
 }

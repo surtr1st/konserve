ad1f0a245bdf2cf17b8dbb9234c6e7d892528e6a
ishi <berugen1st@gmail.com>
2023-09-28 17:38:25 +0700

    convert class sqlite database to function

--- api/src/config/db.ts
+++ api/src/config/db.ts
@@ -1,17 +1,5 @@
 import { Database } from "bun:sqlite";
 
-export class SQLiteDatabase implements Disposable {
-  private db: Database;
-
-  constructor(dbName?: string) {
-    this.db = new Database(dbName);
-  }
-
-  public use(): Database {
-    return this.db;
-  }
-
-  [Symbol.dispose]() {
-    this.db.close();
-  }
+export function createDatabase(name = ":memory:") {
+  return new Database(name);
 }

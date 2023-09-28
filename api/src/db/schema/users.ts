16146f2aa58ea55aba718afbf9d83b111dad86ea
ishi <berugen1st@gmail.com>
2023-09-28 19:08:37 +0700

    fix incorrect column name in user.ts

--- api/src/db/schema/users.ts
+++ api/src/db/schema/users.ts
@@ -1,7 +1,7 @@
 import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable("users", {
-  uid: integer("id").primaryKey(),
+  uid: integer("uid").primaryKey(),
   email: text("email"),
   username: text("username"),
   password: text("password"),

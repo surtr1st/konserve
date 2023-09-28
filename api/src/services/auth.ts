9f83de0c053688e0eee35a56dc95540a5f6be043
ishi <berugen1st@gmail.com>
2023-09-29 02:26:31 +0700

    add partial JWT implementation

--- api/src/services/auth.ts
+++ api/src/services/auth.ts
@@ -2,19 +2,56 @@ import { Elysia } from "elysia";
 import { cookie } from "@elysiajs/cookie";
 import { jwt } from "@elysiajs/jwt";
 
+type AuthParams = {
+  username: string;
+  password: string;
+};
+
+type JWTAuthPayload = {
+  userId: string;
+  username: string;
+};
+
+const MAX_AGE = 86400;
+const JWT_CONFIG = {
+  name: "jwt",
+  secret: "Endless Pain",
+};
+
 export const auth = new Elysia()
-  .use(
-    jwt({
-      name: "jwt",
-      secret: "Endless Pain",
-    }),
-  )
+  .use(jwt(JWT_CONFIG))
   .use(cookie())
-  .get("/sign/:name", async ({ jwt, setCookie, params }) => {
-    const token = await jwt.sign(params);
-    setCookie("auth", token, {
-      httpOnly: true,
-      maxAge: 7 * 86400,
-    });
-    return token;
-  });
+  .post(
+    "/auth",
+    async ({ jwt, body, setCookie }) => {
+      const {} = body as AuthParams;
+      const token = await jwt.sign(JWT_CONFIG);
+      setCookie("jwt.auth", token, {
+        httpOnly: true,
+        maxAge: MAX_AGE,
+      });
+      return token;
+    },
+    {
+      beforeHandle: async ({ jwt, set, request }) => {
+        const authHeader = request.headers.get("authorization");
+        const token = authHeader && authHeader.split(" ")[1];
+
+        if (!token) {
+          set.status = 401;
+          return "Unauthorized";
+        }
+
+        const payload = (await jwt.verify(token)) as JWTAuthPayload;
+        if (!payload) {
+          set.status = 403;
+          return "Invalid token or your token has been expired.";
+        }
+
+        return {
+          message: "Authenticated",
+          user: payload,
+        };
+      },
+    },
+  );

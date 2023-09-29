2aa60aff8348644d30435da0620dd9732a9d090e
ishi <berugen1st@gmail.com>
2023-09-29 07:18:30 +0700

    add node read and create route

--- api/src/models/index.ts
+++ api/src/models/index.ts
@@ -1,1 +1,2 @@
 export * from "./auth.models";
+export * from "./node.models";
--- api/src/routes/node.routes.ts
+++ api/src/routes/node.routes.ts
@@ -1,3 +1,32 @@
 import { Elysia } from "elysia";
+import { useDrizzle } from "../config";
+import { nodes } from "../db/schema";
+import { Node } from "../models";
 
-export const node = new Elysia();
+const db = useDrizzle();
+
+export const node = new Elysia().group("/node", (node) =>
+  node
+    .get("/", async () => await db.select().from(nodes))
+    .post(
+      "/",
+      async ({ body }) => {
+        const { name, userId } = body;
+        return await db.insert(nodes).values({ name, userId });
+      },
+      {
+        body: Node,
+        beforeHandle: ({ set, body }) => {
+          const { name, userId } = body;
+          if (!name) {
+            set.status = 404;
+            return "Please provide the name!";
+          }
+          if (!userId) {
+            set.status = 404;
+            return "Unknown node owner!";
+          }
+        },
+      },
+    ),
+);

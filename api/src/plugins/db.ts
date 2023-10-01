2bef60daeb99bb54480a305a07ce4a1415a2a813
ishi <berugen1st@gmail.com>
2023-10-02 06:21:39 +0700

    update name in db plugin

--- api/src/plugins/db.ts
+++ api/src/plugins/db.ts
@@ -1,7 +1,7 @@
 import { Elysia } from "elysia";
 import { useDrizzle } from "../config";
 
-export const databaseServices = new Elysia({ name: "node@services" }).decorate(
+export const databaseServices = new Elysia({ name: "db@services" }).decorate(
   "db",
   useDrizzle(),
 );

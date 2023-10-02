e5cdc864134c47271b98e011c97736504f1a52b4
ishi <berugen1st@gmail.com>
2023-10-02 20:52:48 +0700

    use derive middlewares change for function middlewares

--- api/src/middlewares/node.ts
+++ api/src/middlewares/node.ts
@@ -1,49 +1,28 @@
-import { OptionalHandler } from "elysia";
-import { NodeDTO } from "$models";
+import { Elysia } from "elysia";
+import { nodeDto } from "$models";
 
-// export const nodeMiddlewares = new Elysia({ name: "node@middlewares" })
-//   .use(nodeDto)
-//   .guard({ body: "node.dto" })
-//   .derive(({ set, body, params }) => {
-//     const validateBody = () => {
-//       const { name, userId } = body;
-//       if (!name || name.length === 0) {
-//         set.status = 404;
-//         return { message: "Please provide the name!" };
-//       }
-//       if (!userId) {
-//         set.status = 404;
-//         return { message: "Unknown node owner!" };
-//       }
-//     };
-//     const isIntParams = () => {
-//       const { id } = params;
-//       const regex = new RegExp(/[0-9]/);
-//       if (!regex.test(id)) {
-//         set.status = 406;
-//         return { message: "Invalid params type!" };
-//       }
-//     };
-//     return { validateBody, isIntParams };
-//   });
-
-export const isIntParams: OptionalHandler<any, any> = ({ params, set }) => {
-  const { id } = params;
-  const regex = new RegExp(/[0-9]/);
-  if (!regex.test(id)) {
-    set.status = 406;
-    throw new Error("Invalid params type!");
-  }
-};
-
-export const validateBody: OptionalHandler<any, any> = ({ body, set }) => {
-  const { name, userId } = body as typeof NodeDTO.properties;
-  if (!name || name.length === 0) {
-    set.status = 404;
-    throw new Error("Please provide the name!");
-  }
-  if (!userId) {
-    set.status = 404;
-    throw new Error("Unknown node owner!");
-  }
-};
+export const nodeMiddlewares = new Elysia({ name: "node@middlewares" })
+  .use(nodeDto)
+  .guard({ body: "node.dto" })
+  .derive(({ set, body, params }) => {
+    const validateBody = () => {
+      const { name, userId } = body;
+      if (!name || name.length === 0) {
+        set.status = 404;
+        return { message: "Please provide the name!" };
+      }
+      if (!userId) {
+        set.status = 404;
+        return { message: "Unknown node owner!" };
+      }
+    };
+    const isIntParams = () => {
+      const { id } = params;
+      const regex = new RegExp(/[0-9]/);
+      if (!regex.test(id)) {
+        set.status = 406;
+        return { message: "Invalid params type!" };
+      }
+    };
+    return { validateBody, isIntParams };
+  });
--- api/src/routes/node.ts
+++ api/src/routes/node.ts
@@ -1,19 +1,23 @@
 import { Elysia } from "elysia";
-import { isIntParams, validateBody } from "$middlewares";
+import { nodeMiddlewares } from "$middlewares";
 import { nodeControllers } from "$controllers";
 
 export const node = new Elysia({ name: "node@routes" })
   .use(nodeControllers)
+  .use(nodeMiddlewares)
   .get("/nodes", ({ getNodes }) => getNodes())
   .group("/node", (node) =>
     node
       .post("/", ({ createNode }) => createNode(), {
-        beforeHandle: validateBody,
+        beforeHandle: ({ validateBody }) => validateBody(),
       })
       .put("/:id", ({ updateNode }) => updateNode(), {
-        beforeHandle: [isIntParams, validateBody],
+        beforeHandle: [
+          ({ isIntParams }) => isIntParams(),
+          ({ validateBody }) => validateBody(),
+        ],
       })
       .delete("/:id", ({ deleteNode }) => deleteNode(), {
-        beforeHandle: isIntParams,
+        beforeHandle: ({ isIntParams }) => isIntParams(),
       }),
   );

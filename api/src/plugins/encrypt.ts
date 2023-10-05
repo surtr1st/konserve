abf13dcd64774244e5b0fa4891a283e45eb0a64a
ishi <berugen1st@gmail.com>
2023-10-06 05:57:10 +0700

    remove cost in hash

--- api/src/plugins/encrypt.ts
+++ api/src/plugins/encrypt.ts
@@ -2,7 +2,7 @@ import Elysia from "elysia";
 
 export const encrypt = new Elysia({ name: "encrypt@services" }).derive(({}) => {
   const hash = async (password: string) =>
-    await Bun.password.hash(password, { cost: 7, algorithm: "bcrypt" });
+    await Bun.password.hash(password, { algorithm: "bcrypt" });
   const isMatch = async (password: string, encryped: string) =>
     await Bun.password.verify(password, encryped);
   return { hash, isMatch };

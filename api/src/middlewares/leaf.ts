033a217126ff79c95e1d0b663f3f2d2192832daf
ishi <berugen1st@gmail.com>
2023-10-06 09:53:20 +0700

    adjust leaf middleware structure and implement verify user secret

--- api/src/middlewares/leaf.ts
+++ api/src/middlewares/leaf.ts
@@ -1,30 +1,48 @@
 import Elysia from "elysia";
 import { leafModel } from "$models";
-import { errorHandlers } from "$plugins";
+import { authTokenRetriever, errorHandlers } from "$plugins";
+import { verification } from "./verification";
 
 export const leafMiddlewares = new Elysia({ name: "leaf@middlewares" })
   .use(leafModel)
   .use(errorHandlers)
+  .use(authTokenRetriever)
+  .use(verification)
   .guard({ body: "leaf.dto" })
-  .derive(({ body, query, validateQueries, validateBodyProps }) => {
-    const verifyRequestBody = () =>
-      validateBodyProps({
-        requestBody: body,
-        requiredKeys: ["username"],
-        responseError: {
-          username: "Please provide the name",
+  .derive(
+    ({
+      body,
+      query,
+      validateQueries,
+      validateBodyProps,
+      useAuthToken,
+      verifySecretCode,
+    }) => {
+      return {
+        verifyRequestBody: () =>
+          validateBodyProps({
+            requestBody: body,
+            requiredKeys: ["username"],
+            responseError: {
+              username: "Please provide the name",
+            },
+          }),
+        verifyRequestQueries: () =>
+          validateQueries({
+            requestQueries: query,
+            requiredKeys: ["id", "nodeId"],
+            responseError: {
+              id: "Unknown leaf!",
+              nodeId: "Unknown leaf owner! Please provide its node!",
+            },
+          }),
+        verifyUserSecret: async () => {
+          const { userId, secretCode } = useAuthToken();
+          await verifySecretCode(
+            parseInt(userId as string),
+            secretCode as string,
+          );
         },
-      });
-
-    const verifyRequestQueries = () =>
-      validateQueries({
-        requestQueries: query,
-        requiredKeys: ["id", "nodeId"],
-        responseError: {
-          id: "Unknown leaf!",
-          nodeId: "Unknown leaf owner! Please provide its node!",
-        },
-      });
-
-    return { verifyRequestBody, verifyRequestQueries };
-  });
+      };
+    },
+  );

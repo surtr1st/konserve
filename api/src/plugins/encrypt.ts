87c2bda1fecde332af14892170007a861819cbff
ishi <berugen1st@gmail.com>
2023-10-06 08:33:27 +0700

    add encrypt, decrypt fn and change from `encrypt` to `crypto`

--- api/src/plugins/encrypt.ts
+++
@@ -1,9 +0,0 @@
-import Elysia from "elysia";
-
-export const encrypt = new Elysia({ name: "encrypt@services" }).derive(({}) => {
-  const hash = async (password: string) =>
-    await Bun.password.hash(password, { algorithm: "bcrypt" });
-  const isMatch = async (password: string, encryped: string) =>
-    await Bun.password.verify(password, encryped);
-  return { hash, isMatch };
-});
--- api/src/plugins/index.ts
+++ api/src/plugins/index.ts
@@ -1,4 +1,4 @@
 export * from "./db";
 export * from "./auth-token-retriever";
 export * from "./error-handler";
-export * from "./encrypt";
+export * from "./crypto";

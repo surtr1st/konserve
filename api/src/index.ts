995ecaa91717f0b95700bad5b6618c54f76423e1
ishi <berugen1st@gmail.com>
2023-10-07 17:36:08 +0700

    test specific origins

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,7 +4,7 @@ import { cors } from "@elysiajs/cors";
 // import { CORS_ORIGIN } from "$config";
 
 new Elysia()
-  .use(cors())
+  .use(cors({ origin: ["http://127.0.0.1:5173", "http://127.0.0.1:5173/"] }))
   .group("/api", (app) => app.use(auth).use(user).use(node).use(leaf))
   .listen(3000);
 

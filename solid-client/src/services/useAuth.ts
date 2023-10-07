b7d4a0a7e702241d8d9d8e95bedea5d7c4df705c
ishi <berugen1st@gmail.com>
2023-10-07 18:45:32 +0700

    change back to `useFetchClient`

--- solid-client/src/services/useAuth.ts
+++ solid-client/src/services/useAuth.ts
@@ -1,25 +1,20 @@
 import { BASE_URL } from '.';
-import { useLocalStore } from '../hooks';
+import { useFetchClient, useLocalStore } from '../hooks';
 
 export function useAuth() {
   const authenticate = ({ username, password }: TAuthParams) => {
-    const [_, setAuth] = useLocalStore<TAuthHeaders>('auth', {});
-    // const { onPost, useDefaultHeaders } = useFetchClient();
-    // onPost<TAuthParams>(
-    //   `${BASE_URL}/auth`,
-    //   {
-    //     headers: useDefaultHeaders({
-    //       authContent: { bearer: auth.bearer as string },
-    //     }),
-    //     credentials: 'include',
-    //   },
-    //   { username, password },
-    fetch(`${BASE_URL}/auth`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username, password }),
-      credentials: 'include',
-    })
+    const [auth, setAuth] = useLocalStore<TAuthHeaders>('auth', {});
+    const { onPost, useDefaultHeaders } = useFetchClient();
+    onPost<TAuthParams>(
+      `${BASE_URL}/auth`,
+      {
+        headers: useDefaultHeaders({
+          authContent: { bearer: auth.bearer as string },
+        }),
+        credentials: 'include',
+      },
+      { username, password },
+    )
       .then(async (res) => {
         const { accessToken: bearer, userId: user } = await res.json();
         setAuth({ bearer, user });

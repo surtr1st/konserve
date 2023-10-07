b3b2cf9f59072a4870d69ec39adead5c9fa57565
ishi <berugen1st@gmail.com>
2023-10-07 17:50:27 +0700

    reconfig cors 4

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,7 +4,13 @@ import { cors } from "@elysiajs/cors";
 import { CORS_ORIGIN } from "$config";
 
 new Elysia()
-  .use(cors({ origin: CORS_ORIGIN || true, preflight: false }))
+  .use(
+    cors({
+      origin: CORS_ORIGIN || true,
+      allowedHeaders: ["Content-Type", "Accept", "Authorization"],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    }),
+  )
   .group("/api", (app) => app.use(auth).use(user).use(node).use(leaf))
   .listen(3000);
 

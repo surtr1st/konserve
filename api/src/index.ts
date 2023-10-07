d015b776d81b48a2da4814693730ae250b61abc6
ishi <berugen1st@gmail.com>
2023-10-07 17:17:50 +0700

    config cors deps

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,11 +3,9 @@ import { auth, leaf, node, user } from "$routes";
 import { cors } from "@elysiajs/cors";
 import { CORS_ORIGIN } from "$config";
 
-const app = new Elysia()
-  .use(cors({ origin: CORS_ORIGIN, credentials: true }))
+new Elysia()
+  .use(cors({ origin: CORS_ORIGIN || true }))
   .group("/api", (app) => app.use(auth).use(user).use(node).use(leaf))
   .listen(3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
-);
+console.log(`🦊 Elysia is running. ${CORS_ORIGIN}`);

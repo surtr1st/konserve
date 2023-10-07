7af75ae1dfa4c94d53fef90694366d4251ee9c28
ishi <berugen1st@gmail.com>
2023-10-07 15:01:06 +0700

    add prefix `/api`

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,9 @@
 import { Elysia } from "elysia";
 import { auth, leaf, node, user } from "$routes";
 
-const app = new Elysia().use(auth).use(user).use(node).use(leaf).listen(3000);
+const app = new Elysia()
+  .group("/api", (app) => app.use(auth).use(user).use(node).use(leaf))
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,

06a37b68bff3d91704b4e1ba54d6526449291f64
ishi <berugen1st@gmail.com>
2023-10-07 17:05:42 +0700

    add `cors` plugin for handling origin

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,10 @@
 import { Elysia } from "elysia";
 import { auth, leaf, node, user } from "$routes";
+import { cors } from "@elysiajs/cors";
+import { CORS_ORIGIN } from "$config";
 
 const app = new Elysia()
+  .use(cors({ origin: CORS_ORIGIN, credentials: true }))
   .group("/api", (app) => app.use(auth).use(user).use(node).use(leaf))
   .listen(3000);
 

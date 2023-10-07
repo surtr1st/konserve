74f1d79631f294a50b5d94b1ed6830171bc5c62c
ishi <berugen1st@gmail.com>
2023-10-07 17:05:23 +0700

    add `CORS_ORIGIN` in env config

--- api/src/config/env.ts
+++ api/src/config/env.ts
@@ -3,3 +3,4 @@ export const TURSO_AUTH_TOKEN = process.env.TURSO_AUTH_TOKEN;
 export const JWT_SECRET = process.env.JWT_SECRET as string;
 export const FILE_XDATA = process.env.FILE_XDATA as string;
 export const FILE_DEST = process.env.FILE_DEST as string;
+export const CORS_ORIGIN = process.env.CORS_ORIGIN as string;

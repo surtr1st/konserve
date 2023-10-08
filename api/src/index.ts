f3c38d9131115c29698436f0d002b8b81b560dd6
ishi <berugen1st@gmail.com>
2023-10-08 09:24:07 +0700

    use wildcard for origin

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,11 @@
 import { Elysia } from "elysia";
 import { auth, leaf, node, user } from "$routes";
 import { cors } from "@elysiajs/cors";
-import { CORS_ORIGIN } from "$config";
-
-const useWildcard = () => true;
 
 new Elysia()
   .use(
     cors({
-      origin: CORS_ORIGIN ? CORS_ORIGIN : () => useWildcard(),
+      origin: () => true,
       allowedHeaders: ["Content-Type", "Accept", "Authorization"],
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
       credentials: true,
@@ -18,4 +15,3 @@ new Elysia()
   .listen(3000);
 
 console.log("🦊 Elysia is running.");
-console.log(CORS_ORIGIN ? CORS_ORIGIN : () => useWildcard());

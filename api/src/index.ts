ef409db9317e613408b669de0d1b454b96f6d48c
ishi <berugen1st@gmail.com>
2023-10-08 10:13:10 +0700

    config origin

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,18 @@
 import { Elysia } from "elysia";
 import { auth, leaf, node, user } from "$routes";
 import { cors } from "@elysiajs/cors";
+import { ALLOWED_ORIGIN } from "$config";
 
 new Elysia()
   .use(
     cors({
-      origin: () => true,
+      origin: (req: Request): boolean => {
+        const origin = req.headers.get("origin");
+        if (!origin) return false;
+        return origin === ALLOWED_ORIGIN;
+      },
       allowedHeaders: ["Content-Type", "Accept", "Authorization"],
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-      credentials: true,
     }),
   )
   .group("/api", (app) => app.use(auth).use(user).use(node).use(leaf))

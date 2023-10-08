b8416f0594b2a96b631ebd7385abb2a62d56cf6f
ishi <berugen1st@gmail.com>
2023-10-08 09:18:27 +0700

    test cors config

--- api/src/index.ts
+++ api/src/index.ts
@@ -8,12 +8,22 @@ const useWildcard = () => true;
 new Elysia()
   .use(
     cors({
-      origin: CORS_ORIGIN || useWildcard(),
+      origin: CORS_ORIGIN ? CORS_ORIGIN : useWildcard(),
       allowedHeaders: ["Content-Type", "Accept", "Authorization"],
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+      credentials: true,
     }),
   )
   .group("/api", (app) => app.use(auth).use(user).use(node).use(leaf))
   .listen(3000);
 
 console.log("🦊 Elysia is running.");
+console.log(CORS_ORIGIN ? CORS_ORIGIN : useWildcard());
+console.log(
+  cors({
+    origin: CORS_ORIGIN ? CORS_ORIGIN : useWildcard(),
+    allowedHeaders: ["Content-Type", "Accept", "Authorization"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    credentials: true,
+  }),
+);

f5a02ed637302274a920f0386bca8ec1489910c8
ishi <berugen1st@gmail.com>
2023-10-08 09:10:13 +0700

    remove `createEffect` in local store hook

--- solid-client/src/hooks/useLocalStore.ts
+++ solid-client/src/hooks/useLocalStore.ts
@@ -1,4 +1,3 @@
-import { createEffect } from 'solid-js';
 import { createStore, SetStoreFunction, Store } from 'solid-js/store';
 
 export function useLocalStore<T extends object>(
@@ -9,7 +8,7 @@ export function useLocalStore<T extends object>(
   const [state, setState] = createStore<T>(
     localState ? JSON.parse(localState) : init,
   );
-  createEffect(() => localStorage.setItem(name, JSON.stringify(state)));
+  localStorage.setItem(name, JSON.stringify(state));
   return [state, setState];
 }
 

ca9e60352ace3ca0a61e676285c018fd2c1cd3eb
ishi <berugen1st@gmail.com>
2023-10-14 05:12:04 +0700

    remove verify token and add handler for verify user

--- api/iris/internal/middlewares/auth.go
+++ api/iris/internal/middlewares/auth.go
@@ -1,13 +1,49 @@
 package middlewares
 
-import "github.com/kataras/iris/v12"
+import (
+	"konserve/api/internal/helpers"
+	"konserve/api/internal/models"
+	"konserve/api/internal/services"
+	"konserve/api/internal/utils"
+
+	"github.com/kataras/iris/v12"
+)
 
 type AuthMiddleware struct{}
 
-func (m AuthMiddleware) VerifyToken(ctx iris.Context) {
-	ctx.Next()
-}
-
 func (m AuthMiddleware) VerifyUser(ctx iris.Context) {
+	var account models.Account
+	err := ctx.ReadJSON(&account)
+	if err != nil {
+		ctx.StopWithJSON(iris.StatusInternalServerError, err)
+		return
+	}
+
+	store := "account"
+	response := map[string]string{"username": USERNAME, "password": PASSWORD}
+	service := services.UserService{DB: utils.UseTurso()}
+	ctx.Values().Set("account", account)
+	handler := helpers.ErrorHandler[models.Account]{Store: store, Response: response}
+	encrypt := utils.Encrypto{}
+	code, message := handler.ValidateBody(ctx)
+
+	if code != 0 {
+		ctx.StopWithJSON(iris.StatusNotFound, iris.Map{"message": message})
+		return
+	}
+
+	user, findErr := service.FindByUsername(account.Username)
+	if findErr != nil {
+		ctx.StopWithJSON(iris.StatusInternalServerError, findErr)
+		return
+	}
+
+	verifyPassword := encrypt.IsMatch([]byte(user.Password), account.Password)
+	if verifyPassword != nil {
+		ctx.StopWithJSON(iris.StatusForbidden, iris.Map{"message": "Password does not match!"})
+		return
+	}
+	ctx.Values().Set("userId", user.Uid)
+
 	ctx.Next()
 }

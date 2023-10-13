bb1505fa2f0f5baaea8a3b6d823b5562956b721b
ishi <berugen1st@gmail.com>
2023-10-13 20:25:41 +0700

    add update and delete service for user

--- api/iris/internal/services/user.go
+++ api/iris/internal/services/user.go
@@ -9,6 +9,8 @@ import (
 const (
   RETRIEVE_USERS  = "SELECT uid, email, username, password, display_name, secret_code FROM users"
   INSERT_USER     = "INSERT INTO users (email, username, password, display_name, secret_code) VALUES (?, ?, ?, ?, ?)"
+  UPDATE_USER     = "UPDATE users SET email = $1 SET username = $2 SET password = $3 SET display_name = $4 SET secret_code = $5 WHERE uid = $6"
+  DELETE_USER     = "DELETE FROM users WHERE uid = ?"
 )
 
 type UserService struct {}
@@ -44,7 +46,7 @@ func (service UserService) Users() ([]models.User, error) {
 func (service UserService) CreateUser(newUser models.User) (int64, error) {
   turso := utils.UseTurso()
 
-  result, err := turso.Exec(INSERT_USER, newUser)
+  result, err := turso.Exec(INSERT_USER, newUser.Email, newUser.Username, newUser.Password, newUser.DisplayName, newUser.SecretCode)
   if err != nil {
     return 0, err
   }
@@ -53,3 +55,30 @@ func (service UserService) CreateUser(newUser models.User) (int64, error) {
 
   return response, nil
 }
+
+func (service UserService) UpdateUser(uid int32, data models.User) (int64, error) {
+  turso := utils.UseTurso()
+
+  result, err := turso.Exec(UPDATE_USER, data.Email, data.Username, data.Password, data.DisplayName, data.SecretCode, uid)
+  if err != nil {
+    return 0, err
+  }
+  response, _ := result.RowsAffected()
+  defer turso.Close()
+
+  return response, nil
+}
+
+func (service UserService) DeleteUser(uid int32) (int64, error) {
+  turso := utils.UseTurso()
+
+  result, err := turso.Exec(DELETE_USER, uid)
+  if err != nil {
+    return 0, err
+  }
+  response, _ := result.RowsAffected()
+  defer turso.Close()
+
+  return response, nil
+}
+

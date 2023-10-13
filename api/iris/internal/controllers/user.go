489b38b36f3036fab1868112f5fc787165fa163a
ishi <berugen1st@gmail.com>
2023-10-13 21:07:46 +0700

    update args for update user controller

--- api/iris/internal/controllers/user.go
+++ api/iris/internal/controllers/user.go
@@ -22,6 +22,7 @@ func (ctrl UserController) RetrieveUsers(ctx iris.Context) {
   ctx.JSON(users)
 }
 
+
 func (ctrl UserController) CreateUser(ctx iris.Context) {
   user := ctx.Values().Get("user").(models.User)
   _, err := service.CreateUser(user)
@@ -33,9 +34,10 @@ func (ctrl UserController) CreateUser(ctx iris.Context) {
   ctx.JSON(message)
 }
 
+
 func (ctrl UserController) UpdateUser(ctx iris.Context) {
   target := ctx.Values().Get("target").(models.User)
-  _, err := service.UpdateUser(target.Uid, target)
+  _, err := service.UpdateUser(target)
   if err != nil {
     ctx.StopWithError(iris.StatusInternalServerError, err)
     return
@@ -43,6 +45,7 @@ func (ctrl UserController) UpdateUser(ctx iris.Context) {
   message := iris.Map { "message": fmt.Sprintf("Updated user %d", target.Uid) }
   ctx.JSON(message)
 }
+
 
 func (ctrl UserController) DeleteUser(ctx iris.Context) {
   userId := ctx.Values().Get("userId").(int32)

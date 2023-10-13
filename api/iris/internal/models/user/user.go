52e1a20614d9b6fd06514b0ba12c0b1a8705813b
ishi <berugen1st@gmail.com>
2023-10-13 16:00:33 +0700

    add `user` model

---
+++ api/iris/internal/models/user/user.go
@@ -0,0 +1,12 @@
+package user
+
+type User struct {
+  Uid         int32     `json:"uid"`
+  Email       string    `json:"email" validate:"required"`
+  Username    string    `json:"username" validate:"required"`
+  Password    string    `json:"password" validate:"required"`
+  DisplayName string    `json:"displayName"`
+  SecretCode  string    `json:"secretCode"`
+}
+
+

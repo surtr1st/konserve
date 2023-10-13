372a4d94cb31f301ffe0d7248a28b6e1d5cd8d99
ishi <berugen1st@gmail.com>
2023-10-13 16:00:47 +0700

    add `leaf` model

---
+++ api/iris/internal/models/leaf/leaf.go
@@ -0,0 +1,9 @@
+package leaf
+
+type Leaf struct {
+  Id         int32     `json:"id"`
+  Username   string    `json:"username" validate:"required"`
+  Password   string    `json:"password" validate:"required"`
+  NodeId     int32     `json:"nodeId" validate:"required"`
+}
+

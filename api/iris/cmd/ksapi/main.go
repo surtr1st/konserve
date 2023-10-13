93dff8cba42d469ed648aedcda564fe76f2553be
ishi <berugen1st@gmail.com>
2023-10-13 20:26:53 +0700

    config cors and test user get and post

--- api/iris/cmd/ksapi/main.go
+++ api/iris/cmd/ksapi/main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
-  "github.com/kataras/iris/v12"
-  "github.com/iris-contrib/middleware/cors"
+	"github.com/kataras/iris/v12"
+	"github.com/iris-contrib/middleware/cors"
+	"konserve/api/internal/controllers"
+	"konserve/api/internal/middlewares"
 ) 
 
 func main() {
   app := iris.New()
   corsOptions := cors.Options {
-    AllowedOrigins: []string { "http://localhost:5173" },
+    AllowedOrigins: []string { "*", "http://localhost:5173" },
     AllowedHeaders: []string { "Content-Type", "Accept", "Authorization" },
     AllowedMethods: []string { "GET", "POST", "PUT", "DELETE", "OPTIONS" },
     AllowCredentials: true,
   }
   api := app.Party("/api")
   api.UseRouter(cors.New(corsOptions))
+
+  user := controllers.UserController {}
+  userMiddleware := middlewares.UserMiddleware {}
+  api.Get("/users", user.RetrieveUsers)
+  api.Post("/user/register", userMiddleware.ValidateBody, user.CreateUser)
   app.Listen(":4000")
 }

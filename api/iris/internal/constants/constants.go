c2a36d40015affdb95d42bb45941e97dcbc0a60a
ishi <berugen1st@gmail.com>
2023-10-13 20:24:44 +0700

    update `constants`

--- api/iris/internal/constants/constants.go
+++ api/iris/internal/constants/constants.go
@@ -1,10 +1,22 @@
 package constants
 
-import "konserve/api/internal/utils/env"
+import (
+	"fmt"
+	"os"
+)
 
 var (
-  TURSO_URL           = env.UseEnv("TURSO_URL")
-  TURSO_AUTH_TOKEN    = env.UseEnv("TURSO_AUTH_TOKEN")
-  ALLOWED_ORIGINS     = env.UseEnv("ALLOWED_ORIGINS")
-  JWT_SECRET          = env.UseEnv("JWT_SECRET")
+  TURSO_URL           = useEnv("TURSO_URL")
+  TURSO_AUTH_TOKEN    = useEnv("TURSO_AUTH_TOKEN")
+  ALLOWED_ORIGINS     = useEnv("ALLOWED_ORIGINS")
+  JWT_SECRET          = useEnv("JWT_SECRET")
 )
+
+
+func useEnv(name string) string {
+  if _, ok := os.LookupEnv(name); !ok {
+    return fmt.Sprintf("No env name `%s` present!", name)
+  }
+  return os.Getenv(name)
+}
+

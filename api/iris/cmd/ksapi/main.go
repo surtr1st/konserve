4e7f4d831d122a1d95691b02af59c84e98a86e72
ishi <berugen1st@gmail.com>
2023-10-13 16:00:04 +0700

    config cors for app

--- api/iris/cmd/ksapi/main.go
+++ api/iris/cmd/ksapi/main.go
@@ -1,8 +1,19 @@
 package main
 
-import "github.com/kataras/iris/v12"
+import (
+  "github.com/kataras/iris/v12"
+  "github.com/iris-contrib/middleware/cors"
+) 
 
 func main() {
   app := iris.New()
+  corsOptions := cors.Options {
+    AllowedOrigins: []string { "http://localhost:5173" },
+    AllowedHeaders: []string { "Content-Type", "Accept", "Authorization" },
+    AllowedMethods: []string { "GET", "POST", "PUT", "DELETE", "OPTIONS" },
+    AllowCredentials: true,
+  }
+  app.Party("/api")
+  app.UseRouter(cors.New(corsOptions))
   app.Listen(":4000")
 }

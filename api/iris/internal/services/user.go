29a2e011763ae302295fa3f5d024486410e952b7
ishi <berugen1st@gmail.com>
2023-10-13 21:07:27 +0700

    update user service by using gorm

--- api/iris/internal/services/user.go
+++ api/iris/internal/services/user.go
@@ -1,84 +1,67 @@
 package services
 
 import (
-	"database/sql"
 	"konserve/api/internal/models"
 	"konserve/api/internal/utils"
 )
 
-const (
-  RETRIEVE_USERS  = "SELECT uid, email, username, password, display_name, secret_code FROM users"
-  INSERT_USER     = "INSERT INTO users (email, username, password, display_name, secret_code) VALUES (?, ?, ?, ?, ?)"
-  UPDATE_USER     = "UPDATE users SET email = $1 SET username = $2 SET password = $3 SET display_name = $4 SET secret_code = $5 WHERE uid = $6"
-  DELETE_USER     = "DELETE FROM users WHERE uid = ?"
-)
-
 type UserService struct {}
+var db = utils.UseTurso()
 
 func (service UserService) Users() ([]models.User, error) {
-  turso := utils.UseTurso()
-  rows, err := turso.Query(RETRIEVE_USERS)
-  if err != nil {
-    return nil, err
-  }
-  defer rows.Close()
-
   var users []models.User
-  t := utils.Ternary {}
-
-  for rows.Next() {
-    var user models.User
-    var displayName sql.NullString
-    var secretCode sql.NullString
-    if err := rows.Scan(&user.Uid, &user.Email, &user.Username, &user.Password, &displayName, &secretCode); err != nil {
-      return nil, err
-    }
-    user.DisplayName = t.AssignAfterCompare(displayName.Valid, displayName.String, "")
-    user.SecretCode = t.AssignAfterCompare(secretCode.Valid, secretCode.String, "")
-
-    users = append(users, user)
+  result := db.Find(&users)
+  err := result.Error
+  
+  if err != nil {
+    return nil, err 
   }
-  defer turso.Close()
-
   return users, nil
 }
 
+
+func (service UserService) FindUser(uid int32) (models.User, error) {
+  var user models.User
+  result := db.Where(models.User { Uid: uid }).Find(&user)
+  err := result.Error
+  if err != nil {
+    return user, err
+  }
+  return user, nil
+}
+
+
 func (service UserService) CreateUser(newUser models.User) (int64, error) {
-  turso := utils.UseTurso()
-
-  result, err := turso.Exec(INSERT_USER, newUser.Email, newUser.Username, newUser.Password, newUser.DisplayName, newUser.SecretCode)
+  result := db.Omit("uid").Create(newUser)
+  err := result.Error
   if err != nil {
     return 0, err
   }
-  response, _ := result.LastInsertId()
-  defer turso.Close()
-
-  return response, nil
+  return result.RowsAffected, nil
 }
 
-func (service UserService) UpdateUser(uid int32, data models.User) (int64, error) {
-  turso := utils.UseTurso()
 
-  result, err := turso.Exec(UPDATE_USER, data.Email, data.Username, data.Password, data.DisplayName, data.SecretCode, uid)
+func (service UserService) UpdateUser(data models.User) (int64, error) {
+  result := db.Save(data)
+  err := result.Error
   if err != nil {
     return 0, err
   }
-  response, _ := result.RowsAffected()
-  defer turso.Close()
-
-  return response, nil
+  return result.RowsAffected, nil
 }
+
 
 func (service UserService) DeleteUser(uid int32) (int64, error) {
-  turso := utils.UseTurso()
+  target, findErr := service.FindUser(uid)
+  if findErr != nil {
+    return 0, findErr
+  }
 
-  result, err := turso.Exec(DELETE_USER, uid)
+  result := db.Delete(target)
+  err := result.Error
   if err != nil {
     return 0, err
   }
-  response, _ := result.RowsAffected()
-  defer turso.Close()
-
-  return response, nil
+  return result.RowsAffected, nil
 }
 

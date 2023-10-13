d42ec134e6655cc46f08c3ecc45da12d716c5d41
ishi <berugen1st@gmail.com>
2023-10-13 21:07:10 +0700

    update sql.DB to gorm.DB

--- api/iris/internal/utils/db.go
+++ api/iris/internal/utils/db.go
@@ -4,14 +4,25 @@ import (
 	"database/sql"
 	"konserve/api/internal/constants"
 
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
 	_ "github.com/libsql/libsql-client-go/libsql"
 )
 
-func UseTurso() *sql.DB {
+func UseTurso() *gorm.DB {
+  config := &gorm.Config {}
   connectionString := constants.TURSO_URL
-  db, err := sql.Open("libsql", connectionString)
+
+  turso, connectErr := sql.Open("libsql", connectionString)
+  if connectErr != nil {
+    panic("Failed to connect Turso!")
+  }
+
+  db, err := gorm.Open(sqlite.Dialector { DriverName: "libsql", Conn: turso }, config)
   if err != nil {
     panic("Failed to connect database!")
   }
+
   return db
 }

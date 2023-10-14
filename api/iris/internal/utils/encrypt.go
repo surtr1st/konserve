0872e6eed51f0f6586e54b75786f926e1904d79a
ishi <berugen1st@gmail.com>
2023-10-14 14:29:11 +0700

    update Ecrypto to Encrypt

--- api/iris/internal/utils/encrypt.go
+++ api/iris/internal/utils/encrypt.go
@@ -2,13 +2,13 @@ package utils
 
 import "golang.org/x/crypto/bcrypt"
 
-type Encrypto struct{}
+type Encrypt struct{}
 
-func (enc Encrypto) IsMatch(compared []byte, original string) error {
+func (enc Encrypt) IsMatch(compared []byte, original string) error {
 	return bcrypt.CompareHashAndPassword(compared, []byte(original))
 }
 
-func (enc Encrypto) Hash(target string) (string, error) {
+func (enc Encrypt) Hash(target string) (string, error) {
 	hashed, err := bcrypt.GenerateFromPassword([]byte(target), bcrypt.DefaultCost)
 	if err != nil {
 		return "", err

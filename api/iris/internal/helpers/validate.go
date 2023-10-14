b8203104767a24ae7cd1565691cd80a16c0b1a3b
ishi <berugen1st@gmail.com>
2023-10-14 14:31:21 +0700

    move IsNumber to Validate helper

--- api/iris/internal/helpers/validate.go
+++ api/iris/internal/helpers/validate.go
@@ -1,7 +1,14 @@
 package helpers
 
+import "regexp"
+
 type Validate struct{}
 
 func (v Validate) IsEmpty(value string) bool {
 	return value == ""
+}
+
+func (v Validate) IsNumber(value string) bool {
+	match, _ := regexp.MatchString("^[0-9]*$", value)
+	return match
 }

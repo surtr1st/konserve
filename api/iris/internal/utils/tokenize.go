9f208fd6e66f489fce2bf6cba915498519258b1e
ishi <berugen1st@gmail.com>
2023-10-14 07:35:39 +0700

    change datatype of TokenClaims userid

--- api/iris/internal/utils/tokenize.go
+++ api/iris/internal/utils/tokenize.go
@@ -2,9 +2,14 @@ package utils
 
 import "github.com/kataras/iris/v12/middleware/jwt"
 
+var (
+	SigKey = []byte(`signature_hmac_secret_shared_key`)
+	EncKey = []byte("GCM_AES_256_secret_shared_key_32")
+)
+
 type TokenClaims struct {
 	jwt.Claims
-	UserId string `json:"userId"`
+	UserId int32 `json:"userId"`
 }
 
 func GenerateToken(signer *jwt.Signer, claims TokenClaims) ([]byte, error) {

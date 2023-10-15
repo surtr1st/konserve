8684f71e3783df685d99c8f0e8d0b8ebdb1f7d14
ishi <berugen1st@gmail.com>
2023-10-15 07:45:18 +0700

    re-structure code pattern for `ternary`

--- api/iris/internal/utils/ternary.go
+++ api/iris/internal/utils/ternary.go
@@ -1,10 +1,32 @@
 package utils
 
-type Ternary[T any] struct{}
-
-func (t Ternary[T]) AssignAfterCondition(ok bool, truthyValue T, falsyValue T) T {
-	if !ok {
-		return falsyValue
-	}
-	return truthyValue
+type ternary[T any] struct {
+	value  T
+	status bool
 }
+
+func UseTernary[T any]() *ternary[T] {
+	return &ternary[T]{}
+}
+
+func (t *ternary[T]) When(condition bool) *ternary[T] {
+	t.status = condition
+	return t
+}
+
+func (t *ternary[T]) Assign(truthyValue T) *ternary[T] {
+	t.value = truthyValue
+	return t
+}
+
+func (t *ternary[T]) Else(falsyValue T) T {
+	if !t.status {
+		t.value = falsyValue
+		return t.value
+	}
+	return t.value
+}
+
+// When(condition)
+// .Assign(true)
+// .Else(false)

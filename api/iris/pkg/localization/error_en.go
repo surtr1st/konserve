751d58cea6f781bbe092e5c8c89061b6d55a0899
ishi <berugen1st@gmail.com>
2023-10-15 14:16:01 +0700

    add localization: `en`

---
+++ api/iris/pkg/localization/error_en.go
@@ -0,0 +1,17 @@
+package localization
+
+// Error Response
+const (
+	EMPTY_STRING          = "The value is not visible."
+	NOT_FOUND             = ""
+	EMAIL_FORMAT_VIOLATED = "Invalid format of email."
+	USERNAME_INCORRECT    = "Invalid or inexistent username."
+	PASSWORD_INCORRECT    = "Password is incorrect! Please re-check your password again."
+)
+
+// Validate Response
+const (
+	MISSING_EMAIL    = "Email is empty! Please provide it!"
+	MISSING_USERNAME = "Username is empty! Please provide it!"
+	MISSING_PASSWORD = "Password is empty!"
+)

fe41645172a620742d67c2ecfe3a0937700622a7
ishi <berugen1st@gmail.com>
2023-10-15 12:09:29 +0700

    remove redundant lines of code in auth controller

--- api/iris/internal/controllers/auth.go
+++ api/iris/internal/controllers/auth.go
@@ -1,10 +1,7 @@
 package controllers
 
 import (
-	"fmt"
-	"konserve/api/internal/helpers"
 	"konserve/api/internal/utils"
-	"strings"
 
 	"github.com/kataras/iris/v12"
 )
@@ -12,14 +9,10 @@ import (
 type AuthController struct{}
 
 func (controller AuthController) Authenticate(ctx iris.Context) {
-	ternary := utils.UseTernary[string]()
-	validate := helpers.UseValidate()
+	validate := utils.UseValidate()
+	authHeader := utils.UseHeaderRetriever(ctx)
 
-	authHeader := fmt.Sprintf("%s %s", ctx.Request().Header.Get("Authorization"), " ")
-	tokenString := strings.Split(authHeader, " ")[1]
-	nilToken := validate.Is(authHeader).Empty()
-	token := ternary.When(nilToken).Assign("").Else(tokenString)
-
+	token := authHeader.BearerToken()
 	if validate.Is(token).Empty() {
 		userId, _ := ctx.Values().GetInt32("userId")
 		accessToken := ctx.Values().GetString("accessToken")

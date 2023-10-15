4dea5b7bbec7e8bcbb51a951040fea6c26313699
ishi <berugen1st@gmail.com>
2023-10-15 09:37:20 +0700

    rename userid to uid in node model

--- api/iris/internal/models/node.go
+++ api/iris/internal/models/node.go
@@ -1,7 +1,7 @@
 package models
 
 type Node struct {
-	Id     int32  `json:"id"`
-	Name   string `json:"name" validate:"required"`
-	UserId int32  `json:"userId" validate:"required"`
+	Id   int32  `json:"id"`
+	Name string `json:"name" validate:"required"`
+	Uid  int32  `json:"uid" validate:"required"`
 }

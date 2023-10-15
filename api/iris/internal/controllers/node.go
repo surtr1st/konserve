12d6f5220d713e131426f8f68230b756b3592e00
ishi <berugen1st@gmail.com>
2023-10-15 09:39:15 +0700

    add create, update and delete node in controller

--- api/iris/internal/controllers/node.go
+++ api/iris/internal/controllers/node.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"konserve/api/internal/models"
 	"konserve/api/internal/services"
 	"konserve/api/internal/utils"
 
@@ -23,4 +24,50 @@ func (controller NodeController) RetrieveNodes(ctx iris.Context) {
 	}
 
 	ctx.JSON(nodes)
+}
+
+func (controller NodeController) CreateNode(ctx iris.Context) {
+	node := ctx.Values().Get("node").(models.Node)
+
+	newNode := models.Node{
+		Name: node.Name,
+		Uid:  node.Uid,
+	}
+
+	if _, err := controller.useService().Create(newNode); err != nil {
+		ctx.StopWithError(iris.StatusInternalServerError, err)
+		return
+	}
+
+	ctx.StatusCode(iris.StatusCreated)
+}
+
+func (controller NodeController) UpdateNode(ctx iris.Context) {
+	target := ctx.Values().Get("node").(models.Node)
+	nodeId, _ := ctx.Values().GetInt32("nodeId")
+
+	foundNode, findErr := controller.useService().Find(nodeId)
+	if findErr != nil {
+		ctx.StopWithError(iris.StatusInternalServerError, findErr)
+		return
+	}
+
+	foundNode.Name = target.Name
+	if _, err := controller.useService().Update(foundNode); err != nil {
+		ctx.StopWithError(iris.StatusInternalServerError, err)
+		return
+	}
+
+	ctx.StatusCode(iris.StatusOK)
+}
+
+func (controller NodeController) DeleteNode(ctx iris.Context) {
+	nodeId, _ := ctx.Values().GetInt32("nodeId")
+
+	if _, err := controller.useService().Delete(nodeId); err != nil {
+		ctx.StopWithError(iris.StatusInternalServerError, err)
+		return
+	}
+
+	ctx.StatusCode(iris.StatusOK)
 }

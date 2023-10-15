2312773aaa379d7814ad271696a58930227d1097
ishi <berugen1st@gmail.com>
2023-10-15 12:07:22 +0700

    modify property of TokenClaims and remove redundancy

--- api/iris/internal/utils/tokenize.go
+++ api/iris/internal/utils/tokenize.go
@@ -2,20 +2,7 @@ package utils
 
 import "github.com/kataras/iris/v12/middleware/jwt"
 
-var (
-	SigKey = []byte(`signature_hmac_secret_shared_key`)
-	EncKey = []byte("GCM_AES_256_secret_shared_key_32")
-)
-
 type TokenClaims struct {
 	jwt.Claims
-	UserId int32 `json:"userId"`
-}
-
-func GenerateToken(signer *jwt.Signer, claims TokenClaims) ([]byte, error) {
-	token, err := signer.Sign(claims)
-	if err != nil {
-		return nil, err
-	}
-	return token, nil
+	Secret string `json:"secret"`
 }

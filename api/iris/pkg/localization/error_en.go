ab8c22bd484647b50800e8c67b778c771e18907d
ishi <berugen1st@gmail.com>
2023-10-15 07:52:03 +0700

    add INVALID TYPE and CONNECTION ERROR in localization

--- api/iris/pkg/localization/error_en.go
+++ api/iris/pkg/localization/error_en.go
@@ -2,16 +2,19 @@ package localization
 
 // Error Response
 const (
-	EMPTY_STRING          = "The value is not visible."
-	NOT_FOUND             = ""
-	EMAIL_FORMAT_VIOLATED = "Invalid format of email."
-	USERNAME_INCORRECT    = "Invalid or inexistent username."
-	PASSWORD_INCORRECT    = "Password is incorrect! Please re-check your password again."
+	EMPTY_STRING              = "The value is not visible."
+	NOT_FOUND                 = ""
+	EMAIL_FORMAT_VIOLATED     = "Invalid format of email."
+	USERNAME_INCORRECT        = "Invalid or inexistent username."
+	PASSWORD_INCORRECT        = "Password is incorrect! Please re-check your password again."
+	TURSO_CONNECTION_ERROR    = "Failed to connect Turso!"
+	DATABASE_CONNECTION_ERROR = "Failed to connect database!"
 )
 
 // Validate Response
 const (
-	MISSING_EMAIL    = "Email is empty! Please provide it!"
-	MISSING_USERNAME = "Username is empty! Please provide it!"
-	MISSING_PASSWORD = "Password is empty!"
+	MISSING_EMAIL       = "Email is empty! Please provide it!"
+	MISSING_USERNAME    = "Username is empty! Please provide it!"
+	MISSING_PASSWORD    = "Password is empty!"
+	INVALID_PARAMS_TYPE = "Params should be a number value!"
 )

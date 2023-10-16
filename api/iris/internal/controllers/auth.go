737e73d93b5efd932f0cd46ea136da5e395f3adf
ishi <berugen1st@gmail.com>
2023-10-16 09:32:30 +0700

    update logic authenticate in auth controller

--- api/iris/internal/controllers/auth.go
+++ api/iris/internal/controllers/auth.go
@@ -10,14 +10,15 @@ type AuthController struct{}
 
 func (controller AuthController) Authenticate(ctx iris.Context) {
 	validate := utils.UseValidate()
-	authHeader := utils.UseHeaderRetriever(ctx)
+	authHeader := utils.UseTokenRetriever(ctx)
 
-	token := authHeader.BearerToken()
+	token := authHeader.AccessToken()
+
 	if validate.Is(token).Empty() {
 		userId, _ := ctx.Values().GetInt32("userId")
 		accessToken := ctx.Values().GetString("accessToken")
-
-		ctx.JSON(iris.Map{"userId": userId, "accessToken": accessToken})
+		ctx.SetCookieKV("accessToken", accessToken)
+		ctx.Writef("%d", userId)
 		return
 	}
 }

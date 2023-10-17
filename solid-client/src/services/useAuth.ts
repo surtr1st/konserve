27a7d3ba52dcfe0156b944aee38d7708720ed089
ishi <berugen1st@gmail.com>
2023-10-17 11:49:16 +0700

    change local to session storage and update auth validate

--- solid-client/src/services/useAuth.ts
+++ solid-client/src/services/useAuth.ts
@@ -1,21 +1,28 @@
 import { BASE_URL } from '.';
-import { useFetchClient, useLocalStore } from '../hooks';
+import { useFetchClient } from '../hooks';
 
 export function useAuth() {
-  const authenticate = ({ username, password }: TAuthParams) => {
-    const [_, setAuth] = useLocalStore<TUserId>('auth', {});
-    const { onPost } = useFetchClient(BASE_URL);
-    onPost<TAuthParams>('/auth', {}, { username, password })
-      .then(async (res) => {
-        const userId = parseInt(await res.text());
-        setAuth({ userId });
-      })
-      .catch((err) => err);
+  const { onGet, onPost } = useFetchClient(BASE_URL);
+  const authenticate = async ({ username, password }: TAuthParams) => {
+    try {
+      const res = await onPost<TAuthParams>(
+        '/auth',
+        {},
+        { username, password },
+      );
+      if (res.status === 404) throw new Error(await res.text());
+      const json = (await res.json()) as TAuthHeaders;
+      sessionStorage.setItem('UserID', json.user as string);
+      sessionStorage.setItem('AccessToken', json.bearer as string);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
-  const isAuth = () => {
-    const [auth, _] = useLocalStore<TUserId>('auth', {});
-    return auth.userId ? true : false;
+  const isAuth = async () => {
+    const accessToken = sessionStorage.getItem('AccessToken');
+    const res = await onGet(`/auth/verify?token=${accessToken}`);
+    return res.status < 400 ? true : false;
   };
 
   return { authenticate, isAuth };

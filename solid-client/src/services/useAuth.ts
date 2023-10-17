555aeeb06c0b3fa9301244dfc3d1d4d1f2a94b20
ishi <berugen1st@gmail.com>
2023-10-17 07:00:21 +0700

    update useAuth

--- solid-client/src/services/useAuth.ts
+++ solid-client/src/services/useAuth.ts
@@ -3,30 +3,19 @@ import { useFetchClient, useLocalStore } from '../hooks';
 
 export function useAuth() {
   const authenticate = ({ username, password }: TAuthParams) => {
-    const [auth, setAuth] = useLocalStore<TAuthHeaders>('auth', {});
-    const { onPost, useDefaultHeaders } = useFetchClient();
-    onPost<TAuthParams>(
-      `${BASE_URL}/auth`,
-      {
-        headers: useDefaultHeaders({
-          authContent: {
-            bearer: auth.bearer as string,
-          },
-        }),
-        credentials: 'include',
-      },
-      { username, password },
-    )
+    const [_, setAuth] = useLocalStore<TUserId>('auth', {});
+    const { onPost } = useFetchClient(BASE_URL);
+    onPost<TAuthParams>('/auth', {}, { username, password })
       .then(async (res) => {
-        const { accessToken: bearer, userId: user } = await res.json();
-        setAuth({ bearer, user });
+        const userId = parseInt(await res.text());
+        setAuth({ userId });
       })
-      .catch((err) => console.error(err));
+      .catch((err) => err);
   };
 
   const isAuth = () => {
-    const [auth, _] = useLocalStore<TAuthHeaders>('auth', {});
-    return auth.bearer || auth.user ? true : false;
+    const [auth, _] = useLocalStore<TUserId>('auth', {});
+    return auth.userId ? true : false;
   };
 
   return { authenticate, isAuth };

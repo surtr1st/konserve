032610c32b547b85330cb6820e30955ce6864a2f
ishi <berugen1st@gmail.com>
2023-10-17 12:34:19 +0700

    add encrypt and decrypt content

--- api/iris/internal/utils/encrypt.go
+++ api/iris/internal/utils/encrypt.go
@@ -1,6 +1,14 @@
 package utils
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/json"
+	"os"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type encrypt struct{}
 
@@ -18,4 +26,80 @@ func (enc encrypt) Hash(target string) (string, error) {
 		return "", err
 	}
 	return string(hashed), nil
+}
+
+func (enc encrypt) Encrypt(file string, secretCode string) ([]byte, error) {
+	// Read the JSON file
+	data, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a new AES cipher block
+	block, err := aes.NewCipher([]byte(secretCode))
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a GCM (Galois/Counter Mode) cipher
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	// Generate a random nonce
+	nonce := make([]byte, aesGCM.NonceSize())
+	if _, err := rand.Read(nonce); err != nil {
+		return nil, err
+	}
+
+	// Encrypt the data
+	encryptedData := aesGCM.Seal(nonce, nonce, data, nil)
+
+	return encryptedData, nil
+}
+
+func (enc encrypt) WriteTo(dest string, data []byte) error {
+	err := os.WriteFile(dest, data, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (enc encrypt) Decrypt(file string, secretCode string) (map[string]interface{}, error) {
+	// Read the encrypted data
+	encryptedData, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a new AES cipher block
+	block, err := aes.NewCipher([]byte(secretCode))
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a GCM (Galois/Counter Mode) cipher
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get the nonce
+	nonceSize := aesGCM.NonceSize()
+	nonce, encryptedData := encryptedData[:nonceSize], encryptedData[nonceSize:]
+
+	// Decrypt the data
+	decryptedData, err := aesGCM.Open(nil, nonce, encryptedData, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var data map[string]interface{}
+	if err := json.Unmarshal(decryptedData, &data); err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }

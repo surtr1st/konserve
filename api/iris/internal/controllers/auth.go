cbc33bcf210bc713fe2f83f8598f7141e317d6b1
ishi <berugen1st@gmail.com>
2023-10-17 11:52:53 +0700

    add verify authentication, and change logic of authenticate

--- api/iris/internal/controllers/auth.go
+++ api/iris/internal/controllers/auth.go
@@ -1,10 +1,11 @@
 package controllers
 
 import (
+	"konserve/api/internal/constants/env"
 	"konserve/api/internal/utils"
-	"time"
 
 	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/middleware/jwt"
 )
 
 type AuthController struct{}
@@ -17,9 +18,17 @@ func (controller AuthController) Authenticate(ctx iris.Context) {
 
 	if validate.Is(token).Empty() {
 		userId, _ := ctx.Values().GetInt32("userId")
-		ctx.AddCookieOptions(iris.CookieHTTPOnly(true), iris.CookieExpires(time.Minute))
-		ctx.SetCookieKV("accessToken", ctx.Values().GetString("accessToken"))
-		ctx.Writef("%d", userId)
+		accessToken := ctx.Values().GetString("accessToken")
+		ctx.JSON(iris.Map{"user": userId, "bearer": accessToken})
+		return
+	}
+}
+
+func (controller AuthController) VerifyAuthorization(ctx iris.Context) {
+	token := ctx.URLParam("token")
+
+	if _, err := jwt.Verify(jwt.HS256, []byte(env.SIGNATURE_KEY), []byte(token)); err != nil {
+		ctx.StopWithStatus(iris.StatusUnauthorized)
 		return
 	}
 }

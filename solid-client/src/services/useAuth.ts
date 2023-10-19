4b0f7641a959f30889c3250c70b1895c76f02504
ishi <berugen1st@gmail.com>
2023-10-19 11:09:44 +0700

    update `authenticate` handler

--- solid-client/src/services/useAuth.ts
+++ solid-client/src/services/useAuth.ts
@@ -4,19 +4,15 @@ import { useFetchClient } from '../hooks';
 export function useAuth() {
   const { onGet, onPost } = useFetchClient(BASE_URL);
   const authenticate = async ({ username, password }: TAuthParams) => {
-    try {
-      const res = await onPost<TAuthParams>(
-        '/auth',
-        {},
-        { username, password },
-      );
-      if (res.status === 404) throw new Error(await res.text());
-      const json = (await res.json()) as TAuthHeaders;
-      sessionStorage.setItem('UserID', json.user as string);
-      sessionStorage.setItem('AccessToken', json.bearer as string);
-    } catch (e) {
-      console.error(e);
-    }
+    const res = await onPost<TAuthParams>('/auth', {}, { username, password });
+
+    if (res.status >= 400) throw new Error(await res.text());
+
+    const json = (await res.json()) as TAuthHeaders;
+    sessionStorage.setItem('UserID', json.user as string);
+    sessionStorage.setItem('AccessToken', json.bearer as string);
+
+    return res.status;
   };
 
   const isAuthorized = async () => {

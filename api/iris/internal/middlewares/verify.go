484509fabc5903eb7ce324379ee2c4846991814f
ishi <berugen1st@gmail.com>
2023-10-20 16:05:25 +0700

    adjust verify count in middleware

--- api/iris/internal/middlewares/verify.go
+++ api/iris/internal/middlewares/verify.go
@@ -14,23 +14,19 @@ type VerifyMiddleware struct{}
 
 func (middleware VerifyMiddleware) Verify(ctx iris.Context) {
 	encrypt := utils.UseEncrypt()
-	ternary := utils.UseTernary[int]()
 	service := services.UserService{DB: utils.UseTurso()}
 
 	userId, _ := ctx.URLParamInt("userId")
 	secretCode := ctx.URLParam("secretCode")
+	verifyCount, _ := ctx.URLParamInt("verifyCount")
 
 	user, err := service.Find(int32(userId))
 	if err != nil {
 		return
 	}
 
-	verifyCount, err := ctx.Values().GetInt("verifyCount")
-
 	hashedSecret, _ := encrypt.Hash(secretCode)
 	if isSecretMatch := encrypt.IsMatch([]byte(hashedSecret), user.SecretCode); isSecretMatch != nil {
-		fmt.Println(verifyCount, verifyCount+1)
-		ctx.Values().Set("verifyCount", ternary.When(err != nil).Assign(1).Else(verifyCount+1))
 		ctx.StopWithError(iris.StatusNotAcceptable, errors.New(fmt.Sprintf("%s Verify Count: %d", locale.MISMATCH_SECRET_CODE, verifyCount)))
 		return
 	}

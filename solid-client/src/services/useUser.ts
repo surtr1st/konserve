5913a3af76029adc90ee7666fe50d617f0411a98
ishi <berugen1st@gmail.com>
2023-10-21 18:55:01 +0700

    handle error for create user

--- solid-client/src/services/useUser.ts
+++ solid-client/src/services/useUser.ts
@@ -4,10 +4,10 @@ import { useFetchClient } from '../hooks';
 export function useUser() {
   const { onPost } = useFetchClient(`${BASE_URL}/users`);
 
-  const createUser = (user: Partial<TUserParams>) => {
-    onPost<Partial<TUserParams>>('/register', {}, user)
-      .then((res) => res.ok)
-      .catch((err) => err);
+  const createUser = async (user: Partial<TUserParams>) => {
+    const res = await onPost<Partial<TUserParams>>('/register', {}, user);
+    if (res.status >= 400) throw new Error(await res.text());
+    return res.ok;
   };
 
   return { createUser };

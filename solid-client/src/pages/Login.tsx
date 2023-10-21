2caa480fb21fc6def0d2ff2819483f4a98e0ced9
ishi <berugen1st@gmail.com>
2023-10-21 17:42:45 +0700

    shorten infer ref type in Login

--- solid-client/src/pages/Login.tsx
+++ solid-client/src/pages/Login.tsx
@@ -8,14 +8,8 @@ export function Login() {
   const navigate = useNavigate();
   const { onSuccess, onError } = useSolidToast();
   const { authenticate, isAuthorized } = useAuth();
-  let username:
-    | HTMLInputElement
-    | ((el: HTMLInputElement) => undefined)
-    | undefined = undefined;
-  let password:
-    | HTMLInputElement
-    | ((el: HTMLInputElement) => undefined)
-    | undefined = undefined;
+  let username: SolidInputRef;
+  let password: SolidInputRef;
 
   const login = () => {
     const unwrapUsername = (username as HTMLInputElement).value;

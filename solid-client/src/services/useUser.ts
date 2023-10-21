8d295ac77296ff969e072137d972110ffcab32e2
ishi <berugen1st@gmail.com>
2023-10-21 20:25:04 +0700

    add isExisted for email and username

--- solid-client/src/services/useUser.ts
+++ solid-client/src/services/useUser.ts
@@ -2,7 +2,7 @@ import { BASE_URL } from '.';
 import { useFetchClient } from '../hooks';
 
 export function useUser() {
-  const { onPost } = useFetchClient(`${BASE_URL}/users`);
+  const { onGet, onPost, useCredentials } = useFetchClient(`${BASE_URL}/users`);
 
   const createUser = async (user: Partial<TUserParams>) => {
     const res = await onPost<Partial<TUserParams>>('/register', {}, user);
@@ -10,5 +10,25 @@ export function useUser() {
     return res.ok;
   };
 
-  return { createUser };
+  const isEmailExisted = async (email: string) => {
+    const token = localStorage.getItem('AccessToken') as string;
+    const res = await onGet(
+      `/exist?email=${email}`,
+      useCredentials(token, 'include'),
+    );
+    if (res.status >= 400) throw new Error(await res.text());
+    return res.ok;
+  };
+
+  const isUsernameExisted = async (username: string) => {
+    const token = localStorage.getItem('AccessToken') as string;
+    const res = await onGet(
+      `/exist?username=${username}`,
+      useCredentials(token, 'include'),
+    );
+    if (res.status >= 400) throw new Error(await res.text());
+    return res.ok;
+  };
+
+  return { createUser, isEmailExisted, isUsernameExisted };
 }

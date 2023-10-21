2e4b0d54b99655f11770fbfed14934cc5c04f51b
ishi <berugen1st@gmail.com>
2023-10-21 17:39:46 +0700

    change return logic of useDynamicGridColumns

--- solid-client/src/hooks/useDynamicGridColumns.ts
+++ solid-client/src/hooks/useDynamicGridColumns.ts
@@ -1,16 +1,14 @@
 export function useDynamicGridColumns(totalItems: number) {
   switch (totalItems) {
     case 1:
-      return 'grid-cols-1';
-    case 2:
       return 'grid-cols-2';
-    case 3:
+    case 2:
       return 'grid-cols-3';
-    case 4:
+    case 3:
       return 'grid-cols-4';
-    case 5:
+    case 4:
       return 'grid-cols-5';
     default:
-      return '';
+      return 'grid-cols-1';
   }
 }
